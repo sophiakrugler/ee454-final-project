449c52f3c9cae0c0885d9caf422ce2551c982606
Jack Carter <71847270+jackrcarter@users.noreply.github.com>
2024-12-10 17:40:49 -0800

    updated convolution8
    uses more for loops to increase readability

--- convolution8.v
+++ convolution8.v
@@ -10,12 +10,14 @@
 // This convolution module will take in an 8x8, and produce a 6x6
 module convolution8#(
     parameter STARTING_SIZE = 8, // this is the initial height & width
-    parameter WINDOW_SIZE = 3    // this is the height & width of the window
+    parameter WINDOW_SIZE = 3,    // this is the height & width of the window
+    parameter ELEMENT_SIZE = 8
     )(
     //input wires for a feature map
     input wire clk;
     input wire rst;
     input wire [511:0] i_featuremap;  // Maximum number of bits for 8x8 image of 8-bit depth
+    input wire [71:0] kernel;
     output wire [287:0] o_featuremap;  // Maximum size of resulting feature_map, 6x6 of 8-bit depth
 
 ); 
@@ -35,6 +37,8 @@ reg [71:0] window_patch;
 // counters used later
 integer i, j, row_offset, column_offset;
 
+reg [128:0] patch_sum;
+
 always @(posedge clk or posedge rst) begin
     // implement rst logic
     // assume positive rst
@@ -44,46 +48,61 @@ always @(posedge clk or posedge rst) begin
     end else begin
         // for each lateral window
         for (i = 0; i < STARTING_SIZE - WINDOW_SIZE + 1; i = i + 1) begin
+            patch_sum = 128'b0; // reset the sum each time
             for (j = 0; j < STARTING_SIZE - WINDOW_SIZE + 1; j = j + 1) begin
                 
                 // ---------- PATCH CREATION ----------//
                 row_offset    = (i*64); // each row is seperated by 64 bits
-                column_offset = (j*8);  // each column is only 8 bits apart
-
-                
-                // TODO: Check if the below code works to correctly address all of the elements, I think I am doing the patch assignment wrong
-                //window_patch[(71-column_offset) -: 8] = i_featuremap[(511 - row_offset - column_offset) -: 8]; // each element
-
+                column_offset = (j*ELEMENT_SIZE);  // each column is only 8 bits apart
 
                 // Assuming starting top left and navigating to the right, then go down 1 and all the way back over left
-                window_patch[71:64] = i_featuremap[511-(row_offset)-(column_offset):504-(row_offset)-(column_offset)]; // top left
-                window_patch[63:56] = i_featuremap[503-(row_offset)-(column_offset):496-(row_offset)-(column_offset)]; // top middle
-                window_patch[55:48] = i_featuremap[495-(row_offset)-(column_offset):488-(row_offset)-(column_offset)]; // top right
-                window_patch[47:40] = i_featuremap[447-(row_offset)-(column_offset):440-(row_offset)-(column_offset)]; // mid left
-                window_patch[39:32] = i_featuremap[439-(row_offset)-(column_offset):432-(row_offset)-(column_offset)]; // mid mid
-                window_patch[31:24] = i_featuremap[431-(row_offset)-(column_offset):424-(row_offset)-(column_offset)]; // mid right
-                window_patch[23:16] = i_featuremap[383-(row_offset)-(column_offset):376-(row_offset)-(column_offset)]; // low left
-                window_patch[15:8]  = i_featuremap[375-(row_offset)-(column_offset):368-(row_offset)-(column_offset)]; // low mid
-                window_patch[7:0]   = i_featuremap[367-(row_offset)-(column_offset):360-(row_offset)-(column_offset)]; // low right
+                for (k = 0; k < WINDOW_SIZE*WINDOW_SIZE; k = k + 1) begin
+                    window_patch[(71-ELEMENT_SIZE*k) -: 8] = i_featuremap[(511 - row_offset - column_offset - ELEMENT_SIZE*k) -: 8]; // each element
 
-                // ---------- PATCH * KERNEL ----------//
+                    // ---------- PATCH * KERNEL ----------//
+                    window_patch[(71-ELEMENT_SIZE*k) -: 8] = window_patch[(71-ELEMENT_SIZE*k) -: 8] * kernel[(71-ELEMENT_SIZE*k) -: 8]; // TODO: CHECK IF THIS CAN BE PLACED HERE
 
-                // TODO: Create a kernel of 8-bit signed values
+                    // ---------- SUM THE PATCH ----------//
+                    patch_sum = patch_sum + window_patch;
 
-                // Multiply Each window_patch element by the corresponding kernel element
-                window_patch[71:64] = ; // top left
-                window_patch[63:56] = ; // top middle
-                window_patch[55:48] = ; // top right
-                window_patch[47:40] = ; // mid left
-                window_patch[39:32] = ; // mid mid
-                window_patch[31:24] = ; // mid right
-                window_patch[23:16] = ; // low left
-                window_patch[15:8]  = ; // low mid
-                window_patch[7:0]   = ; // low right
+                    // THE ABOVE IS EQUIVALENT TO THE BELOW
+
+                    //window_patch[71:64] = i_featuremap[511-(row_offset)-(column_offset):504-(row_offset)-(column_offset)]; // top left
+                    //window_patch[63:56] = i_featuremap[503-(row_offset)-(column_offset):496-(row_offset)-(column_offset)]; // top middle
+                    //window_patch[55:48] = i_featuremap[495-(row_offset)-(column_offset):488-(row_offset)-(column_offset)]; // top right
+                    //window_patch[47:40] = i_featuremap[447-(row_offset)-(column_offset):440-(row_offset)-(column_offset)]; // mid left
+                    //window_patch[39:32] = i_featuremap[439-(row_offset)-(column_offset):432-(row_offset)-(column_offset)]; // mid mid
+                    //window_patch[31:24] = i_featuremap[431-(row_offset)-(column_offset):424-(row_offset)-(column_offset)]; // mid right
+                    //window_patch[23:16] = i_featuremap[383-(row_offset)-(column_offset):376-(row_offset)-(column_offset)]; // low left
+                    //window_patch[15:8]  = i_featuremap[375-(row_offset)-(column_offset):368-(row_offset)-(column_offset)]; // low mid
+                    //window_patch[7:0]   = i_featuremap[367-(row_offset)-(column_offset):360-(row_offset)-(column_offset)]; // low right
 
 
-            end
-        end
+
+                    // Multiply Each window_patch element by the corresponding kernel element
+                    //for (l = 0; l < WINDOW_SIZE*WINDOW_SIZE; l = l+1) begin
+                    //    window_patch[(71-ELEMENT_SIZE*l) -: 8] = window_patch[(71-ELEMENT_SIZE*l) -: 8] * kernel[(71-ELEMENT_SIZE*l) -: 8];
+                    //end
+
+                    // OR
+
+                    //window_patch[71:64] = window_patch[71 -: 8] * kernel[71 -: 8]; // top left
+                    //window_patch[63:56] = window_patch[63 -: 8] * kernel[63 -: 8]; // top middle
+                    //window_patch[55:48] = window_patch[55 -: 8] * kernel[55 -: 8]; // top right
+                    //window_patch[47:40] = window_patch[47 -: 8] * kernel[47 -: 8]; // mid left
+                    //window_patch[39:32] = window_patch[39 -: 8] * kernel[39 -: 8]; // mid mid
+                    //window_patch[31:24] = window_patch[31 -: 8] * kernel[31 -: 8]; // mid right
+                    //window_patch[23:16] = window_patch[23 -: 8] * kernel[23 -: 8]; // low left
+                    //window_patch[15:8]  = window_patch[15 -: 8] * kernel[15 -: 8]; // low mid
+                    //window_patch[7:0]   = window_patch[7 -: 8]  * kernel[7 -: 8];  // low right
+                end
+
+            // TODO: Assign the patch sum to an element of the feature map
+
+            
+
+            end // of a lateral movement
+        end // how a vertical movement
     end
 end
 
